2dc07bd1320587ce0d1b0a028766b8da8366b805
mhoffman88 <guitarmaster901@yahoo.com>
2025-05-28 08:22:20 -0500

    Update annualappraisaltool.py

--- annualappraisaltool.py
+++ annualappraisaltool.py
@@ -393,7 +393,6 @@ if grievance_type == "AWOL - Annual/Sick Leave":
     selected_arguments = []
     
     for desc, info in awol_checkbox_descriptions.items():
-        st.markdown(desc.replace("\n"))
         checked = st.checkbox(desc, key=f"checkbox_{desc}")
         if checked:
             selected_reasons.append(desc)
