4623ba6671d677906e7a009db53f37ae9d0e8006
mhoffman88 <guitarmaster901@yahoo.com>
2025-06-06 08:47:15 -0500

    Update annualappraisaltool.py

--- annualappraisaltool.py
+++ annualappraisaltool.py
@@ -8,7 +8,7 @@ from reportlab.lib.pagesizes import letter
 from textwrap import wrap
 from PyPDF2 import PdfMerger, PdfReader
 from docx import Document as DocxDocument
-import render_awol_issue
+from render_awol_issue
 
 WRAP_WIDTH = 95
 MAX_UPLOADS = 10
