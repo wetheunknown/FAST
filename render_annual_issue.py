542f8a5b1262a08ed21bacecb1742de28bcf8be2
mhoffman88 <guitarmaster901@yahoo.com>
2025-06-06 11:16:54 -0500

    Update render_annual_issue.py

--- render_annual_issue.py
+++ render_annual_issue.py
@@ -1,1 +1,9 @@
 import streamlit as st
+import datetime
+import holidays
+import tempfile
+import os
+from util import draw_wrapped_section, generate_pdf, convert_to_pdf, calculate_fbd
+
+def render_annual():
+  
