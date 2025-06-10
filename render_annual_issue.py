51eb0b40fc199ae6919eeb11d93d90214058fd44
mhoffman88 <guitarmaster901@yahoo.com>
2025-06-10 10:16:40 -0500

    Update render_annual_issue.py

--- render_annual_issue.py
+++ render_annual_issue.py
@@ -3,7 +3,6 @@ import datetime
 import holidays
 import tempfile
 import os
-from datetime import datetime
 from PyPDF2 import PdfMerger
 from util import wrap_text_to_width, draw_wrapped_section, generate_pdf, convert_to_pdf, calculate_fbd
 
