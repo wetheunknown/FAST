52343de030625140342c04ccd809d2d0842ed582
mhoffman88 <guitarmaster901@yahoo.com>
2025-06-12 10:46:32 -0500

    Update render_annual_issue.py

--- render_annual_issue.py
+++ render_annual_issue.py
@@ -90,8 +90,8 @@ def render_annual():
         dept_man = st.text_input("Department Manager")
         flmanager = st.text_input("Frontline Manager")
         position = st.text_input("Title/Position")
-        issue_description = st.text_area("Summary of Grievance", key="issue_description")
-        desired_outcome = st.text_area("Requested Resolution", key="desired_outcome")
+        issue_description = st.text_area("Summary of Grievance", "")
+        desired_outcome = st.text_area("Requested Resolution", "")
 
         uploaded_files = []
         MAX_UPLOADS = 10
