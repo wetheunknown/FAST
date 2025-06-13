f843176924cca3e626a7c69f7e2bf5e7c4bebfec
wetheunknown <39310330+wetheunknown@users.noreply.github.com>
2025-06-12 22:19:00 -0500

    Update render_template_issue.py
    test

--- render_template_issue.py
+++ render_template_issue.py
@@ -359,7 +359,7 @@ def render_template():
                 "Articles of Violation": article_list
             }
             
-            grievance_type = st.session_state.get("grievance_type", "Test Grievance")
+            grievance_type = st.session_state.get("grievance_type", "Awol Grievance")
     
             cover_sheet = create_cover_sheet(form_data, grievance_type)
             awol_pdf = generate_pdf(pdf_data, full_argument)  # Should return a BytesIO!
