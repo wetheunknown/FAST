bf5b9a35ad0629549d2883a96e079cd274a2a932
wetheunknown <39310330+wetheunknown@users.noreply.github.com>
2025-06-25 20:38:57 -0500

    Update fast_main.py

--- fast_main.py
+++ fast_main.py
@@ -10,7 +10,6 @@ from PyPDF2 import PdfMerger, PdfReader
 from docx import Document as DocxDocument
 from render_awol_issue import render_awol
 from render_annual_issue import render_annual
-from render_template_issue import render_template
 from render_test import render_test
 from util import wrap_text_to_width,draw_wrapped_section, generate_pdf, convert_to_pdf, calculate_fbd
 
@@ -31,9 +30,6 @@ if grievance_type == "Annual Appraisal":
 
 if grievance_type == "AWOL - Annual/Sick Leave":
     render_awol()
-
-if grievance_type == "Test Template":
-    render_template()
     
 if grievance_type == "Test Template":
     render_test()
