e02ed22f2e3a6c6a82c1ac7d274caed7e6915825
mhoffman88 <guitarmaster901@yahoo.com>
2025-06-27 15:46:06 -0500

    Update render_awol_issue.py

--- render_awol_issue.py
+++ render_awol_issue.py
@@ -3,6 +3,7 @@ import datetime
 import holidays
 import tempfile
 import os
+from io import BytesIO
 from PyPDF2 import PdfMerger
 from util import wrap_text_to_width, draw_wrapped_section, generate_pdf, convert_to_pdf, calculate_fbd, create_cover_sheet, merge_pdfs
 
